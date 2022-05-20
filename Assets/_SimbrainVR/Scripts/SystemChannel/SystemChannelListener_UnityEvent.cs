79a027215e4d758bb0eec5f3f85ec51483b65dec
ulissesnascim <ulissesnascim@gmail.com>
2022-05-20 12:45:34 -0700

    Add voice recognition as a module, add several updates to Mind Palace

---
+++ Assets/_SimbrainVR/Scripts/SystemChannel/SystemChannelListener_UnityEvent.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SystemChannelListener_UnityEvent: MonoBehaviour
+{
+    [SerializeField] private bool initializeOnSceneStart = true;
+    [SerializeField] private bool activateOnEnable = false;
+    [SerializeField] private bool deactivateOnDisable = false;
+
+    [SerializeField] private SystemChannelBaseSO systemChannel = default;
+
+    [SerializeField] private UnityEvent<SystemChannelBaseSO> unityEvent = default;
+
+    private void Awake()
+    {
+        if (initializeOnSceneStart)
+            InitializeListener();   
+    }
+
+    private void OnEnable()
+    {
+        if (activateOnEnable)
+            InitializeListener();
+    }
+
+    private void OnDisable()
+    {
+        if (deactivateOnDisable)
+            DisableListener();
+    }
+
+    private void OnDestroy()
+    {
+        DisableListener();
+    }
+
+    public void InitializeListener()
+    {
+        systemChannel.OnEventRaised -= HandleEventRaised;
+        systemChannel.OnEventRaised += HandleEventRaised;
+
+    }
+    public void DisableListener()
+    {
+        systemChannel.OnEventRaised -= HandleEventRaised;
+
+    }
+
+
+    private void HandleEventRaised(SystemChannelBaseSO channel)
+    {
+        unityEvent?.Invoke(channel);
+
+    }
+}
---
+++ Assets/_SimbrainVR/Scripts/SystemChannel/SystemChannelListener_UnityEvent_MultipleChannels.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SystemChannelListener_UnityEvent_MultipleChannels : MonoBehaviour
+{
+    [SerializeField] private bool initializeOnSceneStart = true;
+    [SerializeField] private bool activateOnEnable = false;
+    [SerializeField] private bool deactivateOnDisable = false;
+
+    [SerializeField] private List<SystemChannelBaseSO> requiredSystemChannelsToRaiseEvent = default;
+
+    [SerializeField] private UnityEvent<SystemChannelBaseSO> onAnyChannelRaisedAllowingDuplicate = default;
+    [SerializeField] private UnityEvent<SystemChannelBaseSO> onAllChannelsRaised = default;
+
+    private List<SystemChannelBaseSO> requiredChannelsChecklist = new List<SystemChannelBaseSO>();
+
+    private void Awake()
+    {
+        if (initializeOnSceneStart)
+            InitializeListener();   
+    }
+
+    private void OnEnable()
+    {
+        if (activateOnEnable)
+            InitializeListener();
+    }
+
+    private void OnDisable()
+    {
+        if (deactivateOnDisable)
+            DisableListener();
+    }
+
+    private void OnDestroy()
+    {
+        DisableListener();
+    }
+
+    public void InitializeListener()
+    {
+        requiredChannelsChecklist = new List<SystemChannelBaseSO>(requiredSystemChannelsToRaiseEvent);
+
+        foreach (SystemChannelBaseSO systemChannel in requiredSystemChannelsToRaiseEvent)
+        {
+            systemChannel.OnEventRaised -= HandleEventRaised;
+            systemChannel.OnEventRaised += HandleEventRaised;
+
+        }
+
+    }
+    public void DisableListener()
+    {
+        foreach (SystemChannelBaseSO systemChannel in requiredSystemChannelsToRaiseEvent)
+        {
+            systemChannel.OnEventRaised -= HandleEventRaised;
+
+        }
+
+    }
+
+
+    private void HandleEventRaised(SystemChannelBaseSO channel)
+    {
+        requiredChannelsChecklist.Remove(channel);
+
+        onAnyChannelRaisedAllowingDuplicate?.Invoke(channel);
+
+        if (requiredChannelsChecklist.Count == 0)
+            onAllChannelsRaised?.Invoke(channel);
+
+    }
+}
